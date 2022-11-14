70754704da72b2d99c609de5522e1b99335f03fb
DanOrta <109227419+DanOrta@users.noreply.github.com>
2022-11-14 17:05:57 -0600

    1. The file .gitignore was updated to stop tracking the following files: *.exe, *.o, *.stackdump. 2. The file tshirts.c was updated with more TCs that make the current implementation fail.

--- tshirts.c
+++ tshirts.c
@@ -14,9 +14,14 @@ char size(int cms) {
 }
 
 int main() {
+    assert(size(0) == 'S');
+    assert(size(-1) == 'S');
     assert(size(37) == 'S');
+    assert(size(38) == 'S');
     assert(size(40) == 'M');
+    assert(size(42) == 'M');
     assert(size(43) == 'L');
+    assert(size(0xFFFFFFFF) == 'L');
     printf("All is well (maybe!)\n");
     return 0;
 }
