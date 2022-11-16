c1242c9e58df3f14fcac92c6583559c184f58831
DanOrta <109227419+DanOrta@users.noreply.github.com>
2022-11-16 14:38:50 -0600

    misaligned.c updated with the test case to verify the misalignment

--- misaligned.c
+++ misaligned.c
@@ -5,9 +5,13 @@ int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
+    char colorMap[20] = {};
+
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            sprintf(colorMap, "%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            assert(colorMap[3] == '|');
+            printf("%s", colorMap);
         }
     }
     return i * j;
